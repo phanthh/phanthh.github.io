ccb80327c0e7b0c6e1325d2eca78bd1602f4ed56
phantah1 <phanthehauah1@gmail.com>
2021-02-06 20:45:48 +0700

    Automatic Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!!</h1>
+      <h1>Work</h1>
+      <Navbar />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        atque saepe quae recusandae sit sint ab, rem libero quasi a corrupti
+        exercitationem nulla earum sunt rerum, sequi optio, amet dolorem?
+      </p>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam,
+        temporibus? Dolor fugit officiis accusantium excepturi at ipsam rerum
+        animi adipisci quo magni eligendi quaerat voluptatibus quisquam,
+        voluptates quas possimus earum!
+      </p>
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet
+        ex soluta quod officia natus eligendi minus similique quasi recusandae
+        modi doloribus suscipit porro incidunt consequatur, quibusdam nobis sint
+        mollitia.
+      </p>
     </div>
   );
 }
